311b72146c0da40b2523dfa114b42bccdec71b1d
saloni1966 <salonisharma1966@gmail.com>
2021-05-07 07:05:18 +0530

    2nd part of the story

--- story.js
+++ story.js
@@ -1,2 +1,4 @@
 Once upon a time there was a crocodile who lived with his wife ad a monkey.
 The Monkey had sweet berries and the crcodile's wife wanted them.
+The crocodile brought the monkey gift and asked for the berries.
+Then the monkey gae the crocodile the berries.
